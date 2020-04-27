e861e9467579634e33e576fe4b410db38d60f037
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-27 18:04:37 -0400

    added some minor routing

--- comake/src/App.js
+++ comake/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 //components
-import {Login} from "./components/LoginPage"
+import {Login} from "./components/LoginPage";
+import {Home} from "./components/HomePage";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Login/>
+      <Switch>
+      <Route path="/" component={Login}/>
+      <Route path="/home" component={Home}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
