94dd3a420417a82fe9248a1f3f6aac4831202970
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-27 13:19:41 -0400

    Set up state tracking for inputs, added some flavortext too

--- comake/src/components/LoginPage.js
+++ comake/src/components/LoginPage.js
@@ -7,23 +7,38 @@ export const Login = () => {
         password: ''
     })
 
+    const inputHandler = (event) => {
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return (
         <div>
+            <h1>Welcome to Comake</h1>
+            <h3>Please sign in below, and start reaching out!</h3>
             <form>
                 <input 
                     type="email"
                     placeholder="Email:"
                     name="email"
+                    value={credentials.email}
+                    onChange={inputHandler}
                 /><br/>
                 <input 
                     type="text"
                     placeholder="Username:"
                     name="username"
+                    value={credentials.username}
+                    onChange={inputHandler}
                 /><br/>
                 <input 
                     type="password"
                     placeholder="Password:"
                     name="password"
+                    value={credentials.password}
+                    onChange={inputHandler}
                 /><br/>
                 <input type="submit" />
             </form>
